bc65dbddd55a0f083ca759938bcd6f669bf05017
Juma Paul <raisben7@gmail.com>
2026-08-14 13:43:39 -0700

    feat(server): add HTTP server with /health endpoint

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
-func main() {}
+func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
+
+	log.Println("listening on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
